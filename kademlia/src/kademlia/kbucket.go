de7613a60db9b72375832e6acb46f1b9b383c556
Nevil George <nevilgeorge2016@u.northwestern.edu>
2015-04-21 01:29:37 -0500

    refactoring code, changing array to slice. Code working how it was earlier

--- kademlia/src/kademlia/kbucket.go
+++ kademlia/src/kademlia/kbucket.go
@@ -11,48 +11,76 @@ import (
 
 // KBucket struct
 type KBucket struct {
-	NodeId ID
-	NumContacts int
-	ContactList [k]Contact
+	NodeID ID
+	ContactList []Contact
 	Kad *Kademlia
-	BitMap [k]bool
 }
 
-// Initialize KBuckets, called 160 times when Kademlia is instantiated in kademlia.org
+// Initialize KBuckets, called 160 times when Kademlia is instantiated in kademlia.go
 func (kb *KBucket) Initialize() {
 	fmt.Println("Initialize")
-	kb.NumContacts = 0
-	kb.NodeId = NewRandomID()
-	for i := 0; i < 20; i++ {
-		nullContact := new(Contact)
-		kb.ContactList[i] = *nullContact
-		kb.BitMap[i] = false
-	}
+	kb.NodeID = NewRandomID()
+	// create slice for ContactList
+	kb.ContactList = make([]Contact, k)
 }
 
 // Remove the contact corresponding to a given ID from the KBucket
 func (kb *KBucket) RemoveContact(targetID ID) (bool) {
 	fmt.Println("RemoveContact")
 	for i,_ := range kb.ContactList {
-		if kb.ContactList[i].NodeID == targetID && kb.BitMap[i] == true  {
-			nullContact := new(Contact)
-			kb.ContactList[i] = *nullContact
-			kb.BitMap[i] = false
+		if kb.ContactList[i].NodeID == targetID {
+			temp := kb.ContactList
+			a := append(temp[:i], temp[(i + 1):]...)
+			kb.ContactList = a
 			return true
 		}
 	}
 	return false
 }
 
-// Adds a given contact to a free space in the KBucket
+// Adds a given contact to the end of the kbucket
 func (kb *KBucket) AddContact(newContact Contact) {
 	fmt.Println("AddContact")
-	for i,_ := range kb.ContactList {
-		if kb.BitMap[i] == false {
-			kb.ContactList[i] = newContact
-			kb.BitMap[i] = true
-			break
-		}
-	}
+	kb.ContactList = append(kb.ContactList, newContact)
 }
 
+// returns a boolean for whether a given Contact exists in the KBucket and index if it was found
+func (kb *KBucket) ContainsContact(cont Contact) (exists bool, index int) {
+	// iterate through ContactList and compare Contact NodeIDs
+	for i := 0; i < len(kb.ContactList); i++ {
+		current := kb.ContactList[i]
+		if current.NodeID.Equals(cont.NodeID) {
+			exists = true
+			index = i
+			return
+		}
+	}
+	exists = false
+	index = -1
+	return
+}
+
+// Update the KBucket to sort the nodes with most recently used in at the head of the KBucket
+// func (kb *KBucket) Update(updated Contact) {
+// 	// check whether the updated contact exists in the KBucket
+// 	exists, index := ContainsContact(updated)
+// 	if exists {
+// 		// move Contact to the end of the KBucket
+// 		kb.moveToTail(updated, index)
+// 	} else if len(kb.ContactList) < k {
+// 		// create a new contact for the node and add it to the tail of the KBucket
+// 		// not sure if a new Contact needs to be created, but that's what the doc says
+// 		temp := Contact()
+// 		temp.NodeID = CopyID(updated.NodeID)
+// 		temp.Host = updated.Host
+// 		temp.Port = updated.Port
+// 		kb.appendToTail(temp)
+// 	} else {
+
+// 	}
+// }
+
+// moves a contact from its position in the KBucket to the end of the same KBucket
+// func (kb *KBucket) moveToTail(updated Contact, index int) {
+
+// }
